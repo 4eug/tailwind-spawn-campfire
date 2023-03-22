2d093f5b832de260ea6e1592eef53169e946595e
4eug <debraheug@gmail.com>
2023-03-22 08:21:51 +0000

    Font Config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import localFont from 'next/font/local';
+import type { AppProps } from 'next/app';
+
+const helvetica = localFont({
+  src: [
+    {
+      path: '../../public/fonts/helveticaneueltcom-bdcn-webfont.woff2',
+      weight: '700',
+    },
+    {
+      path: '../../public/fonts/helveticaneueltcom-bdcn-webfont.woff2',
+      weight: '900',
+    },
+  ],
+  variable: '--font-helvetica',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={helvetica.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
